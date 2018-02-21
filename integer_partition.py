d1b5c9418c5d25105e0ac49d2417a2b0c8724d1e
Andrew H. Pometta <apomet@gmail.com>
2018-02-21 00:01:20 -0800

    added minimum necessary comments

--- integer_partition.py
+++ integer_partition.py
@@ -1,11 +1,21 @@
+"""This file, written by Andrew H. Pometta on 2/20/2018, is a Python 3 program
+for solving Project Euler problem #207 on HackerRank.  The problem's 
+description can be found at:
+https://www.hackerrank.com/contests/projecteuler/challenges/euler/207
+
+The readme file in this project explains the problem, solution and plan in 
+more detail.  Please consult the readme for explanations for certain decisions.
+"""
+
 import sys
 
 file_lines = sys.stdin.readlines()
-num_queries = int(file_lines[0])
-del file_lines[0]
+del file_lines[0] #we don't need to know the number of queries
+
+#first step: get list of fractions and place in list from file
+#goal_fract stores the smallest one, so we know when to stop calculating later
 fract_list = []
 goal_fract = 1.1
-#first step: get list of fractions
 for line in file_lines:
     space_ind = line.find(" ")
     a = int(line[:space_ind])
@@ -15,14 +25,19 @@ for line in file_lines:
         goal_fract = fract
     fract_list.append(fract)
 
-#now we build the list
+#now we build the list of tuples, (purity_ratio, corresponding k).  Only
+#append to k_list when we find a purity_ratio smaller than the smallest current
+#one, since otherwise there's no point in storing it.  Note that two_2 is
+#2^t and the variable we incremement by - see readme.
 two_t = 2
 k_list = []
-#insert reasoning later - faster way of figuring out if partition is pure
+#whenever two_2 is a power of 2, we know we have a pure partition.  it's faster
+#to just keep track of the next time we will have one rather than to calculate
+#t with log base 2 and check for integer status every time.
 next_2_pow = 2
 partitions = 0
 pure_partitions = 0
-smallest_fract = 1.1
+smallest_fract = 1.1 #the smallest P(m) so far
 while two_t >= 2: #break this loop manually
     partitions += 1
     if two_t == next_2_pow:
@@ -36,6 +51,9 @@ while two_t >= 2: #break this loop manually
         break
     two_t += 1
 
+
+#With the way the code works, k_list is in decreasing order of purity ratio,
+#and increasing order of k.
 #last step: print out requests
 for n in fract_list:
     for purity in k_list:
