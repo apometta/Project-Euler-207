a3ad0d35d379adc10d941f93204233a1a3442b1e
Andrew H. Pometta <apomet@gmail.com>
2018-02-21 00:17:35 -0800

    c++ initial commit

---
+++ integer_partition.cpp
@@ -0,0 +1,7 @@
+/*This file, written by Andrew H. Pometta on 2/21/2018, is a C++ program for 
+solving Project Euler problem #207, found on HackerRank at:
+https://www.hackerrank.com/contests/projecteuler/challenges/euler207
+
+This is the C++ version, in the hopes that it will go faster.  Please see 
+the readme file in this project for more details on the problem, plan, 
+solution, etc. */
