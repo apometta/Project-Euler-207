fad335cc042c623b436137bb2143c5ff043593ad
apometta <apomet@gmail.com>
2018-02-23 12:40:07 -0800

    finished formatting

--- integer_partition.cpp
+++ integer_partition.cpp
@@ -1,10 +1,14 @@
-/*This file, written by Andrew H. Pometta on 2/21/2018, is a C++ program for 
-solving Project Euler problem #207, found on HackerRank at:
-https://www.hackerrank.com/contests/projecteuler/challenges/euler207
+/*Author: Andrew H. Pometta
+  Last updated: 2/23/2018
 
-This is the C++ version, in the hopes that it will go faster.  Please see 
-the readme file in this project for more details on the problem, plan, 
-solution, etc. */
+  This file is the main file for the Project Euler #207 project.  It uses
+  the RatioKCalculator found in ratio_k_calculator.hpp and implemented in
+  ratio_k_calculator.cpp.  The program takes in FROM THE STANDARD INPUT
+  (i.e. NOT from an argument file, due to the specifications in the problem)
+  a number on a line corresponding to a list of queries, followed by that
+  number of queries on each line.  Each query has two integers a and b.
+  For details, see the readme or the problem page at:
+  https://www.hackerrank.com/contests/projecteuler/challenges/euler207 */
 
 #include <iostream>
 #include <cstdlib>
@@ -13,6 +17,10 @@ using namespace std;
 
 int main(){
   RatioKCalculator calc;
+  /*Since the problem doesn't test for checking input formatting and is really
+    clear about it, I don't do any checking for proper formatting.  Don't run
+    the program on one that is not properly formatted, lest it run until
+    manually terminated.  I may add checks later. */
   int q;
   cin >> q;
   for (int i = 0; i < q; ++i){
@@ -27,4 +35,3 @@ int main(){
   }
   return EXIT_SUCCESS;
 }
-
